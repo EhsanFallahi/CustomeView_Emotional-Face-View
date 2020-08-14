cd8d1f0697c9ebc3f10bc73ed4b988803c734908
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-08-14 12:43:16 +0430

    add listener for button

--- app/src/main/java/com/example/emotionalfacecustomeview/MainActivity.kt
+++ app/src/main/java/com/example/emotionalfacecustomeview/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.emotionalfacecustomeview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 1
+        happyButton.setOnClickListener {
+            emotional_face_view.happinessState=EmotionalFaceView.HAPPY
+        }
+        // 2
+        sadButton.setOnClickListener {
+            emotional_face_view.happinessState = EmotionalFaceView.SAD
+        }
+
     }
 }

421f606ac426dd365b5490eedc0e36aa7588d34b
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-08-14 10:59:57 +0430

    Draw the face background

--- app/src/main/java/com/example/emotionalfacecustomeview/EmotionalFaceView.kt
+++ app/src/main/java/com/example/emotionalfacecustomeview/EmotionalFaceView.kt
@@ -1,9 +1,57 @@
 package com.example.emotionalfacecustomeview
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class EmotionalFaceView(context:Context,attrs:AttributeSet): View(context,attrs) {
+    // Paint object for coloring and styling
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    // Some colors for the face background, eyes and mouth.
+    private var faceColor = Color.YELLOW
+    private var eyesColor = Color.BLACK
+    private var mouthColor = Color.BLACK
+    private var borderColor = Color.BLACK
+    // Face border width in pixels
+    private var borderWidth = 4.0f
+    // View size in pixels
+    private var size = 320
 
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawFaceBackground(canvas)
+        drawEyes(canvas)
+        drawMouth(canvas)
+    }
+    private fun drawFaceBackground(canvas: Canvas) {
+        // 1
+        paint.color = faceColor
+        paint.style = Paint.Style.FILL
+
+        // 2
+        val radius = size / 2f
+
+        // 3
+        canvas.drawCircle(size / 2f, size / 2f, radius, paint)
+
+        // 4
+        paint.apply {
+            color = borderColor
+            style = Paint.Style.STROKE
+            strokeWidth = borderWidth
+        }
+
+
+        // 5
+        canvas.drawCircle(size / 2f, size / 2f, radius - borderWidth / 2f, paint)
+    }
+
+    private fun drawEyes(canvas: Canvas) {
+    }
+
+    private fun drawMouth(canvas: Canvas) {
+    }
 }

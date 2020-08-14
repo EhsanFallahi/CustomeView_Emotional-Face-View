b39b88e0e92ed72d14e62d0e510b21e2962ff282
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-08-14 10:47:43 +0430

    add basic code

---
+++ app/src/main/java/com/example/emotionalfacecustomeview/EmotionalFaceView.kt
@@ -0,0 +1,9 @@
+package com.example.emotionalfacecustomeview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class EmotionalFaceView(context:Context,attrs:AttributeSet): View(context,attrs) {
+
+}
